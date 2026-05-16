8787a7eef343ef0ca2d7b2dc3d6bef2d674affde
Mrseatree <378139419@qq.com>
2026-05-16 23:18:00 +0800

    结构框架更新到五关

---
+++ src/data/levels/Level5.js
@@ -0,0 +1,19 @@
+const level5 = {
+
+    playerSpawn: {
+        x: 100,
+        y: 300
+    },
+
+    npc: {
+        x: 600,
+        y: 200
+    },
+
+    dialogue: [
+        '“主管今天心情不好。”',
+        '“小心一点。”'
+    ]
+};
+
+export default level5;
--- src/data/levels/index.js
+++ src/data/levels/index.js
@@ -1,9 +1,15 @@
 import level1 from './level1';
 import level2 from './level2';
+import level3 from './level3';
+import level4 from './Level4';
+import level5 from './Level5';
 
 const levels = {
     1: level1,
-    2: level2
+    2: level2,
+    3: level3,
+    4: level4,
+    5: level5
 };
 
 export default levels;

2b445b1300a3f3f644a5b6f6a0c74a2e03935e50
Mrseatree <378139419@qq.com>
2026-05-17 18:49:50 +0800

    modified:   src/gameObjects/npcs/npcs.js

--- src/gameObjects/npcs/npcs.js
+++ src/gameObjects/npcs/npcs.js
@@ -1,11 +1,14 @@
 import V2 from './V2';
 import Federico from './Federico';
+import Lemuen from './Lemuen';
 
 const npcMap = {
 
     v2: V2,
 
-    federico: Federico
+    federico: Federico,
+
+    lemuen: Lemuen
 };
 
 export default npcMap;
